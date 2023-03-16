75617c1c105429790cf16eff8e3388d365de9599
REZA DYSTA SATRIA <87264688+dyzzta@users.noreply.github.com>
2023-03-16 23:08:10 +0300

    Update app.ts

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -12,3 +12,21 @@ myPet = "cat";
 myPet = 1234;
 
 //we use "typescript" so we better avoid the "any" type data
+
+
+//if u want to make variable to multidata(string or number)
+
+let myFood : number | string ; //it is unlimited
+
+
+myFood = "1 Yarim ekmek arasi tavuk";
+
+//array  
+    //string[], number[], any[]
+
+let myGirl : string[];
+myGirl = ["Sandy Cheeks","Mikasa Ackerman","Shizuka"];
+
+//tuple array
+let myEx : [string,number,boolean];
+myEx = ["gabaghta",22,true];
